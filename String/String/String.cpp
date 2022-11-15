7a44d20ed60cfa956ad1d29955fda33d1c01c136
UlianaVladko <113168785+UlianaVladko@users.noreply.github.com>
2022-11-15 21:36:45 +0300

    Add files via upload

---
+++ String/String/String.cpp
@@ -0,0 +1,28 @@
+﻿#include<Windows.h>
+#include <iostream>
+#include<string>
+using namespace std;
+
+void print(string str) {
+    cout << str << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    string str;
+    cout << "Введите набор символов" << endl;
+    cin >> str;
+
+    print(str.substr(1, 3));
+
+    int index = str.find("a");
+
+    if (index == -1) {
+        cout << "Символа 'a' не найдено" << endl;
+    }
+    else {
+        print(to_string(index));
+    }
+}
